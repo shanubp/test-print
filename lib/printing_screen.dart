a79e85bae9c6abade8c0678bdbb345e077a8ba2f
shanub <shanub882@gamil.com>
2025-01-26 13:14:04 +0530

    usb printing

--- lib/printing_screen.dart
+++ lib/printing_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:blue_thermal_printer/blue_thermal_printer.dart';
-import 'package:esc_pos_printer_plus/esc_pos_printer_plus.dart';
 import 'package:esc_pos_utils_plus/esc_pos_utils_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -10,6 +9,7 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:image/image.dart' as img;
 import 'package:url_launcher/url_launcher.dart';
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -23,17 +23,18 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     _getDevicelist();
-    _getBluetoothDeviceList();
     // TODO: implement initState
     super.initState();
   }
 
   FlutterUsbPrinter flutterUsbPrinter = FlutterUsbPrinter();
-  BlueThermalPrinter printer = BlueThermalPrinter.instance;
+
+  // BlueThermalPrinter printer = BlueThermalPrinter.instance;
 
   bool connected = false;
   List<Map<String, dynamic>> devices = [];
-  List<BluetoothDevice> bluetoothDevices = [];
+
+  // List<BluetoothDevice> bluetoothDevices = [];
 
 
   ScreenshotController screenController = ScreenshotController();
@@ -45,7 +46,8 @@ class _MyHomePageState extends State<MyHomePage> {
     List<Map<String, dynamic>> results = [];
     results = await FlutterUsbPrinter.getUSBDeviceList();
     for (var device in results) {
-     await _connect(int.parse(device['vendorId']), int.parse(device['productId']));
+      await _connect(
+          int.parse(device['vendorId']), int.parse(device['productId']));
     }
     if (mounted) {
       setState(() {
@@ -55,73 +57,73 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   // usb connection
-   _connect(int vendorId, int productId) async {
-     try {
-       bool? result = await flutterUsbPrinter.connect(vendorId, productId);
-       if (result == true) {
-         setState(() {
-           connected = true;
-         });
-       }
-     } catch (e) {
-       print("Error connecting USB device: $e");
-     }
+  _connect(int vendorId, int productId) async {
+    try {
+      bool? result = await flutterUsbPrinter.connect(vendorId, productId);
+      if (result == true) {
+        setState(() {
+          connected = true;
+        });
+      }
+    } catch (e) {
+      print("Error connecting USB device: $e");
+    }
   }
 
 
-  // get bluetooth device
-  _getBluetoothDeviceList() async {
-    if (await Permission.bluetoothScan.request().isGranted &&
-        await Permission.bluetoothConnect.request().isGranted ) {
-      List<BluetoothDevice> devices = await printer.getBondedDevices();
-      setState(() {
-        bluetoothDevices = devices;
-      });
-    } else {
-      print("Bluetooth permissions not granted");
-    }
-    }
+  // // get bluetooth device
+  // _getBluetoothDeviceList() async {
+  //   if (await Permission.bluetoothScan.request().isGranted &&
+  //       await Permission.bluetoothConnect.request().isGranted ) {
+  //     List<BluetoothDevice> devices = await printer.getBondedDevices();
+  //     setState(() {
+  //       bluetoothDevices = devices;
+  //     });
+  //   } else {
+  //     print("Bluetooth permissions not granted");
+  //   }
+  //   }
+  //
+  //  // bluetooth connection
+  // Future<bool> _connectBluetooth(BuildContext context) async {
+  //   if (await printer.isConnected ?? false) {
+  //     return true;
+  //   }
+  //
+  //   if (bluetoothDevices.isNotEmpty) {
+  //     BluetoothDevice? selectedDevice = await showDialog<BluetoothDevice>(
+  //       context: context,
+  //       builder: (context) {
+  //         return AlertDialog(
+  //           title: const Text("Select Printer"),
+  //           content: Column(
+  //             mainAxisSize: MainAxisSize.min,
+  //             children: bluetoothDevices.map((device) {
+  //               return ListTile(
+  //                 title: Text(device.name ?? "Unknown"),
+  //                 subtitle: Text(device.address ?? "No address"),
+  //                 onTap: () => Navigator.pop(context, device),
+  //               );
+  //             }).toList(),
+  //           ),
+  //         );
+  //       },
+  //     );
+  //
+  //     if (selectedDevice != null) {
+  //       await printer.connect(selectedDevice);
+  //       return true;
+  //     } else {
+  //       print("No device selected!");
+  //     }
+  //   } else {
+  //     print("No paired devices found!");
+  //   }
+  //
+  //   return false;
+  //   }
 
-   // bluetooth connection
-  Future<bool> _connectBluetooth(BuildContext context) async {
-    if (await printer.isConnected ?? false) {
-      return true;
-    }
-
-    if (bluetoothDevices.isNotEmpty) {
-      BluetoothDevice? selectedDevice = await showDialog<BluetoothDevice>(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text("Select Printer"),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: bluetoothDevices.map((device) {
-                return ListTile(
-                  title: Text(device.name ?? "Unknown"),
-                  subtitle: Text(device.address ?? "No address"),
-                  onTap: () => Navigator.pop(context, device),
-                );
-              }).toList(),
-            ),
-          );
-        },
-      );
-
-      if (selectedDevice != null) {
-        await printer.connect(selectedDevice);
-        return true;
-      } else {
-        print("No device selected!");
-      }
-    } else {
-      print("No paired devices found!");
-    }
-
-    return false;
-    }
-
-  Future<void> getPrint() async {
+  Future getUsbPrint() async {
     final CapabilityProfile profile = await CapabilityProfile.load();
     final generator = Generator(PaperSize.mm80, profile);
     List<int> bytes = [];
@@ -144,7 +146,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
     if (captureImage.isNotEmpty) {
       final img.Image image = img.decodeImage(captureImage)!;
-     final img.Image resizedImage = img.copyResize(
+      final img.Image resizedImage = img.copyResize(
           image, width: 480, maintainAspect: false);
 
       // bytes += generator.drawer(pin: PosDrawer.pin2);
@@ -156,52 +158,54 @@ class _MyHomePageState extends State<MyHomePage> {
 
       final Uint8List uint8ListBytes = Uint8List.fromList(bytes);
       print(uint8ListBytes);
-    }
+
+
   }
 
-  //   if (captureImage.isNotEmpty) {
-  //     // Decode the image
-  //     final img.Image image = img.decodeImage(captureImage)!;
-  //
-  //     // Resize the image if necessary
-  //     img.Image thumbnail = img.copyResize(
-  //         image, width: 480, maintainAspect: false);
-  //
-  //     // Convert image to Uint8List
-  //     Uint8List thumbnailBytes = Uint8List.fromList(img.encodePng(thumbnail));
-  //
-  //     // Request storage permission
-  //     final status = await Permission.storage.request();
-  //     if (status.isGranted) {
-  //       // Save to the public Downloads directory
-  //       String downloadPath = '/storage/emulated/0/Download'; // Path to Downloads folder
-  //       String filePath = '$downloadPath/captured_image_${DateTime
-  //           .now()
-  //           .millisecondsSinceEpoch}.png';
-  //
-  //       final File file = File(filePath);
-  //       await file.writeAsBytes(thumbnailBytes);
-  //
-  //       print('Image saved to $filePath');
-  //       // Open the folder
-  //       openFolder(downloadPath); // Open the folder
-  //       return filePath; // Return the file path
-  //     } else {
-  //       print('Permission denied to access storage.');
-  //       return null; // Return null if permission is denied
-  //     }
-  //   }
-  //   return null;
-  // }
-  //
-  // void openFolder(String folderPath) async {
-  //   final url = 'file://$folderPath';
-  //   if (await canLaunch(url)) {
-  //     await launch(url);
-  //   } else {
-  //     print("Cannot open folder.");
-  //   }
-  // }
+
+    if (captureImage.isNotEmpty) {
+      // Decode the image
+      final img.Image image = img.decodeImage(captureImage)!;
+
+      // Resize the image if necessary
+      img.Image thumbnail = img.copyResize(
+          image, width: 480, maintainAspect: false);
+
+      // Convert image to Uint8List
+      Uint8List thumbnailBytes = Uint8List.fromList(img.encodePng(thumbnail));
+
+      // Request storage permission
+      final status = await Permission.storage.request();
+      if (status.isGranted) {
+        // Save to the public Downloads directory
+        String downloadPath = '/storage/emulated/0/Download'; // Path to Downloads folder
+        String filePath = '$downloadPath/captured_image_${DateTime
+            .now()
+            .millisecondsSinceEpoch}.png';
+
+        final File file = File(filePath);
+        await file.writeAsBytes(thumbnailBytes);
+
+        print('Image saved to $filePath');
+        // Open the folder
+        openFolder(downloadPath); // Open the folder
+        return filePath; // Return the file path
+      } else {
+        print('Permission denied to access storage.');
+        return null; // Return null if permission is denied
+      }
+    }
+    return null;
+  }
+
+  void openFolder(String folderPath) async {
+    final url = 'file://$folderPath';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      print("Cannot open folder.");
+    }
+  }
 
 
 
@@ -216,6 +220,7 @@ class _MyHomePageState extends State<MyHomePage> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
+                  maxLines: 4,
                   controller: content,
                   decoration: InputDecoration(
                     enabledBorder: OutlineInputBorder(
@@ -232,7 +237,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               TextButton(
                 onPressed: () {
-                  getPrint();
+                  getUsbPrint();
                 },
                 child: Text('Submit'),)
             ],
